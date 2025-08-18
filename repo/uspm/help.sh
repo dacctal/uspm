b9bc36c9f64be5b35d70ee1caaf5073a1ac18108
dacctal <120422854+dacctal@users.noreply.github.com>
2025-08-18 12:28:25 -0400

    fixing(?) self-updates

---
+++ repo/uspm/help.sh
@@ -0,0 +1,6 @@
+echo "uspm: universal source-based package manager
+Usage:
+  i [PACKAGE-NAME] - install a package
+  r [PACKAGE-NAME] - remove a package
+  u - update all packages
+"
---
+++ repo/uspm/install.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.local/share/uspm/sources/uspm/ &&
+  git clone https://github.com/dacctal/uspm.git ~/.local/share/uspm/sources/uspm/ &&
+  cd ~/.local/share/uspm/sources/uspm/ &&
+  ./build.sh &&
+  cp uspm ~/.local/share/uspm/bin/uspm &&
+  cd -
