3325e11bd7c084948700b9f3d3e3c9985d9fe0b3
dacctal <120422854+dacctal@users.noreply.github.com>
2025-08-18 14:18:43 -0400

    no more weird recursive ignores

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 gcc main.c -o uspm
+mv uspm ~/.local/share/uspm/bin/
+
 cp -r ./repo ~/.local/share/uspm/
+
 if ! [ -d "~/.local/share/uspm/bin/" ]; then
   mkdir ~/.local/share/uspm/bin/
 fi
