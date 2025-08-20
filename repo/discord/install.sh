f5e072de7c473a1c305d9772568c71e40875d1ca
dacctal <120422854+dacctal@users.noreply.github.com>
2025-08-19 23:05:13 -0400

    added discord to repo

---
+++ repo/discord/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+Package="discord"
+Sources="$HOME/.local/share/uspm/sources/$Package"
+Bin="$HOME/.local/share/uspm/bin/"
+
+rm -rf "$Sources"
+
+mkdir -p "$Sources"
+cd "$Sources"
+curl -L "https://discord.com/api/download?platform=linux&format=tar.gz" -o discord.tar.gz
+
+tar -xvzf discord.tar.gz
+echo "#!/bin/sh
+~/.local/share/uspm/sources/discord/Discord/Discord" >>discord
+chmod +x discord
+cp discord "$Bin"
+cp . /opt/discord
+
+cd -
