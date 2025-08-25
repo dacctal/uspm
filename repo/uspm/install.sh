59b1411e2b15fdcf52d1bebadc6a14889eec44a3
dacctal <120422854+dacctal@users.noreply.github.com>
2025-08-25 03:28:48 -0400

    test uspm install script

--- repo/uspm/install.sh
+++ repo/uspm/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-source $SCRIPT_DIR/../config.sh
 source <(curl -s https://raw.githubusercontent.com/dacctal/uspm/refs/heads/main/repo/config.sh)
 Package=$(basename "$SCRIPT_DIR")
 
--- repo_maintain/templates/remove.sh
+++ repo_maintain/templates/remove.sh
@@ -2,7 +2,9 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 source $SCRIPT_DIR/../config.sh
+source <(curl -s https://raw.githubusercontent.com/dacctal/uspm/refs/heads/main/repo/config.sh)
 source $SCRIPT_DIR/./builds.sh
+source <(curl -s https://raw.githubusercontent.com/dacctal/uspm/refs/heads/main/repo/$Package/builds.sh)
 
 remove_package
 
